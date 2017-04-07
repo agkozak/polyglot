6a0e3ea2cb258c906909b61ca8492ac23922a6dc
Alexandros Kozák <git-agkozak@sneakemail.com>
2017-04-07 09:28:44 -0700

    Documentation and licensing

--- agkozak-git-prompt.sh
+++ agkozak-git-prompt.sh
@@ -7,6 +7,23 @@
 #
 # agkozak Git Prompt
 #
+# A Dynamic color Git prompt for zsh, bash, ksh, mksh, and dash
+#
+# Copyright (C) 2017 Alexandros Kozák
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+#
 # https://github.com/agkozak/agkozak-git-prompt
 #
 
@@ -73,7 +90,7 @@ if [ -n "$ZSH_VERSION" ]; then
     autoload -Uz colors
     colors
   fi
- 
+
   # vi-mode indicator
   #
   # Underscores are used in the new keymap's name to keep `dash` from choking on hyphens
