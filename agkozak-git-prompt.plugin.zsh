9355819d44493efd546cc10ffef6c5d8b6af86dd
Alexandros Kozak <git-agkozak@sneakemail.com>
2017-04-11 12:09:40 -0700

    zsh syntax in boostrap script

--- agkozak-git-prompt.plugin.zsh
+++ agkozak-git-prompt.plugin.zsh
@@ -30,7 +30,7 @@
 # shellcheck disable=SC2148
 
 # Compile agkozak-git-prompt.sh with zcompile when necessary
-if [ "${0:a:h}/agkozak-git-prompt.sh" -nt "${0:a:h}/agkozak-git-prompt.sh.zwc" ] || [ ! -e "${0:a:h}/agkozak-git-prompt.sh.zwc" ]; then
+if [[ ${0:a:h}/agkozak-git-prompt.sh -nt ${0:a:h}/agkozak-git-prompt.sh.zwc ]] || [[ ! -e ${0:a:h}/agkozak-git-prompt.sh.zwc ]]; then
   zcompile "${0:a:h}/agkozak-git-prompt.sh"
 fi
 
