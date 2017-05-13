b950517cb115f30ed1a6acd828d52b00499e4c83
Alexandros Kozak <git-agkozak@sneakemail.com>
2017-05-12 21:03:11 -0700

    Documentation

--- polyglot.plugin.zsh
+++ polyglot.plugin.zsh
@@ -1,33 +1,4 @@
-#              _             _       _
-#  _ __   ___ | |_   _  __ _| | ___ | |_
-# | '_ \ / _ \| | | | |/ _` | |/ _ \| __|
-# | |_) | (_) | | |_| | (_| | | (_) | |_
-# | .__/ \___/|_|\__, |\__, |_|\___/ \__|
-# |_|            |___/ |___/
-#
-# Polyglot Prompt
-#
-# A dynamic color Git prompt for zsh, bash, ksh93, mksh, pdksh, dash, and
-# busybox sh
-#
-#
-# Copyright (C) 2017 Alexandros Kozák
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-#
-# https://github.com/agkozak/polyglot
-#
+# Polyglot Prompt shim for zsh frameworks
 
 # shellcheck disable=SC2148
 
