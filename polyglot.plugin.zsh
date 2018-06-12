cd9b2c6d01dbcdab794c31251ac1ae6bb3eaa3e2
Alexandros Kozák <git-agkozak@sneakemail.com>
2018-06-12 11:56:34 -0700

    Resolve all symlinks

--- polyglot.plugin.zsh
+++ polyglot.plugin.zsh
@@ -3,9 +3,9 @@
 # shellcheck disable=SC2148
 
 # Compile polyglot.sh with zcompile when necessary
-if [[ ${0:a:h}/polyglot.sh -nt ${0:a:h}/polyglot.sh.zwc ]] || [[ ! -e ${0:a:h}/polyglot.sh.zwc ]]; then
-  zcompile "${0:a:h}/polyglot.sh"
+if [[ ${0:A:h}/polyglot.sh -nt ${0:A:h}/polyglot.sh.zwc ]] || [[ ! -e ${0:A:h}/polyglot.sh.zwc ]]; then
+  zcompile "${0:A:h}/polyglot.sh"
 fi
 
 #shellcheck source=/dev/null
- . "${0:a:h}/polyglot.sh"
+ . "${0:A:h}/polyglot.sh"
