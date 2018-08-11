6f3aacc1fb1cb737e9ef708e6ce5976383330102
Alexandros Kozak <git-agkozak@sneakemail.com>
2018-08-11 05:06:58 +0000

    Color support for pdksh

--- polyglot.sh
+++ polyglot.sh
@@ -490,10 +490,19 @@ elif _polyglot_is_pdksh || [ "$0" = 'dash' ] || _polyglot_is_busybox; then
     POLYGLOT_HOSTNAME_STRING=''
   fi
 
-  if ! _polyglot_is_superuser; then
-    PS1='$(_polyglot_exit_status $?)${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING $(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")$(_polyglot_branch_status) $ '
-  else  # Superuser
-    PS1='$(_polyglot_exit_status $?)$(tput rev)${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING$(tput sgr0) $(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")$(_polyglot_branch_status) $ '
+  if _polyglot_is_pdksh && _polyglot_has_colors; then
+    x=$(print \\001)
+    if ! _polyglot_is_superuser; then
+      PS1="$x$(print "\\r$x\033[31;1m$x$(print '$(_polyglot_exit_status $?)')$x\033[0m\033[32;1m$x${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING$x\033[0m$x $x\033[34;1m$x$(print '$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")')$x\033[0m\033[33m$x$( print '$(_polyglot_branch_status)')$x\033[0m$x \$ ")"
+    else
+      PS1="$x$(print "\\r$x\033[31;1m$x$(print '$(_polyglot_exit_status $?)')$x\033[0m\033[7m$x${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING$x\033[0m$x $x\033[34;1m$x$(print '$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")')$x\033[0m\033[33m$x$(print '$(_polyglot_branch_status)')$x\033[0m$x \$ ")"
+    fi
+  else
+    if ! _polyglot_is_superuser; then
+      PS1='$(_polyglot_exit_status $?)${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING $(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")$(_polyglot_branch_status) $ '
+    else  # Superuser
+      PS1='$(_polyglot_exit_status $?)$(tput rev)${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING$(tput sgr0) $(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")$(_polyglot_branch_status) $ '
+    fi
   fi
 
 else
