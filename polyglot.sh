618004f673b0acb50e698d2f39556a23bd9e939c
Alexandros Kozak <git-agkozak@sneakemail.com>
2020-07-06 15:15:45 -0700

    Restore '@' before hostname in ZSH

--- polyglot.sh
+++ polyglot.sh
@@ -420,7 +420,7 @@ if [ "$ZSH_VERSION" ] && [ "$0" != 'ksh' ] \
 
   # Only display the $HOSTNAME for an ssh connection, except for a superuser
   if _polyglot_is_ssh || _polyglot_is_superuser; then
-    psvar[1]=${HOST%%\.*}
+    psvar[1]="@${HOST%%\.*}"
   else
     psvar[1]=''
   fi
