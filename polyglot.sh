6ac5156a871f6682c38e4cdaec71e209359ec207
Jason Pena <jasonpena@awkless.com>
2022-08-19 15:11:13 -0600

    feat: Add support for the yash shell
    I wanted to use the `polyglot.sh` script for my current working shell
    yash, but the script stated that my shell was not supported. So i
    decided to add support for it.
    
    I did my best to adhere to the coding style, and prompt style of the
    script. The only thing lacking is the ability to show what Vi mode you
    are in since yash does not support that (I looked through its manual
    and there is no mention of such a feature at all).
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- polyglot.sh
+++ polyglot.sh
@@ -376,6 +376,14 @@ _polyglot_sh_is_dash() {
   esac
 }
 
+_polyglot_is_yash()
+{
+  case "${0#-}" in
+    *yash) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
 ###########################################################
 # Output virtual environment name
 ###########################################################
@@ -674,11 +682,11 @@ elif [ -n "$KSH_VERSION" ] || _polyglot_is_dtksh || [ -n "$ZSH_VERSION" ] &&
   esac
 
 ####################################################################
-# pdksh, dash, busybox ash, and zsh in sh emulation mode
+# pdksh, dash, busybox ash, yash, and zsh in sh emulation mode
 ####################################################################
 
 elif _polyglot_is_pdksh || [ "${0#-}" = 'dash' ] || _polyglot_is_busybox ||
-  _polyglot_sh_is_dash; then
+  _polyglot_is_yash || _polyglot_sh_is_dash; then
 
   # Only display the $HOSTNAME for an ssh connection
   if _polyglot_is_ssh || _polyglot_is_superuser; then
@@ -686,6 +694,41 @@ elif _polyglot_is_pdksh || [ "${0#-}" = 'dash' ] || _polyglot_is_busybox ||
     POLYGLOT_HOSTNAME_STRING="@${POLYGLOT_HOSTNAME_STRING%%\.*}"
   else
     POLYGLOT_HOSTNAME_STRING=''
+  fi
+ 
+  # Configure the yash shell, which is similar to dash but with quality of life
+  # features like tab completion. The only thing yash lacks is the ability to
+  # show what mode you are in when using its rendition of Vi-mode.
+  if _polyglot_is_yash; then
+    if _polyglot_is_superuser; then
+      if _polyglot_has_colors; then
+        YASH_PS1='\[\e[01;31m\]$(_polyglot_exit_status $?)\[\e[0m\]'
+        YASH_PS1=$YASH_PS1'$(_polyglot_venv)'
+        YASH_PS1=$YASH_PS1'\[\e[01;32m\]${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING\[\e[0m\] '
+        YASH_PS1=$YASH_PS1'\[\e[01;34m\]$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")\[\e[0m\]'
+        YASH_PS1=$YASH_PS1'\[\e[33m\]$(_polyglot_branch_status $POLYGLOT_KSH_BANG)\[\e[0m\] # '
+      else
+        YASH_PS1='$(_polyglot_exit_status $?)'
+        YASH_PS1=$YASH_PS1'$(_polyglot_venv)'
+        YASH_PS1=$YASH_PS1'${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING '
+        YASH_PS1=$YASH_PS1'$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")'
+        YASH_PS1=$YASH_PS1'$(_polyglot_branch_status $POLYGLOT_KSH_BANG) # '
+      fi
+    else
+      if _polyglot_has_colors; then
+        YASH_PS1='\[\e[01;31m\]$(_polyglot_exit_status $?)\[\e[0m\]'
+        YASH_PS1=$YASH_PS1'$(_polyglot_venv)'
+        YASH_PS1=$YASH_PS1'\[\e[01;32m\]${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING\[\e[0m\] '
+        YASH_PS1=$YASH_PS1'\[\e[01;34m\]$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")\[\e[0m\]'
+        YASH_PS1=$YASH_PS1'\[\e[33m\]$(_polyglot_branch_status $POLYGLOT_KSH_BANG)\[\e[0m\] \$ '
+      else
+        YASH_PS1='$(_polyglot_exit_status $?)'
+        YASH_PS1=$YASH_PS1'$(_polyglot_venv)'
+        YASH_PS1=$YASH_PS1'${LOGNAME:-$(logname)}$POLYGLOT_HOSTNAME_STRING '
+        YASH_PS1=$YASH_PS1'$(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")'
+        YASH_PS1=$YASH_PS1'$(_polyglot_branch_status $POLYGLOT_KSH_BANG) \$ '
+      fi
+    fi
   fi
 
   # pdksh uses a non-printing character of the programmer's choice to delimit
@@ -759,7 +802,6 @@ elif _polyglot_is_pdksh || [ "${0#-}" = 'dash' ] || _polyglot_is_busybox ||
     _polyglot_is_pdksh && ! _polyglot_is_dragonfly_console && PS1=$PS1$(print "$POLYGLOT_NP")
     PS1=$PS1' $(_polyglot_prompt_dirtrim "$POLYGLOT_PROMPT_DIRTRIM")$(_polyglot_branch_status $POLYGLOT_KSH_BANG) # '
   fi
-
 else
   printf '%s\n' 'Polyglot Prompt does not support your shell.' >&2
 fi
